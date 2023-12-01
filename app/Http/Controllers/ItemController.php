48d0d9c1c40e5c9c8116f91e0bc66f50d7ce1cc9
peacher108 <220025da@yse-c.net>
2023-12-01 12:20:12 +0900

    change

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
@@ -11,7 +12,13 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        // TODO: データをすべて取得
+        // SELECT * FROM items;
+        $items = Item::get();
+        $data['items'] = $items;
+
+        // views/item/index.blade.php
+        return view('item.index', $data);
     }
 
     /**
@@ -19,7 +26,8 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        // views/item/create.blade.php
+        return view('item.create');
     }
 
     /**
@@ -27,7 +35,15 @@ class ItemController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // dd($request);
+        // dd($request->all());
+        //Requestからデータを取得
+        $data = $request->all();
+        //データベースに保存
+        // INSERT INTO items (name, price) VALUES (xxxx, xxxx);
+        Item::create($data);
+        //リダイレクト
+        return redirect(route('item.index'));
     }
 
     /**
@@ -58,24 +74,44 @@ class ItemController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(int $id)
     {
-        //
+        //商品IDから商品データを取得
+        // SELECT * FROM items WHERE id = xx;
+        $item = Item::find($id);
+        $data['item'] = $item;
+        //編集画面を表示
+        return view('item.edit', $data);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, int $id)
     {
-        //
+        $data = $request->all();
+        // dd($data);
+        // UPDATE items SET price = xxx WHERE id = xx;
+        // 1.
+        // unset($data['_token']);
+        // Item::where('id', $id)->update($data);
+        // 2.
+        // SELECT * FROM items WHERE id = xx;
+        // UPDATE items SET price = xxx WHERE id = xx;
+        Item::find($id)->fill($data)->save();
+
+        //リダイレクト
+        return redirect(route('item.edit', $id));
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(int $id)
     {
-        //
+        // DELETE FROM items WHERE id = xx;
+        Item::destroy($id);
+        // 一覧画面にリダイレクト
+        return redirect(route('item.index'));
     }
 }
--- resources/views/item/show.blade.php
+++ resources/views/item/show.blade.php
@@ -3,10 +3,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h2>About</h2>
-                    <div>
-                        This is About Page.
-                    </div>
+                    <h2>商品情報</h2>
+                    <h3>商品名</h3>
+                    <p>
+                        {{ $item }}
+                    </p>
                 </div>
             </div>
         </div>
